95ba6929cce469a3f842924956cb1d83f1b0740d
aneeshraj05 <aneesh.rajx@gmail.com>
2026-03-10 12:50:05 +0530

    dib fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,14 +4,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Aneesh@123',
-  database: process.env.DB_NAME || 'expenses',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10
 });
-
 // Test connection on startup
 db.getConnection((err, connection) => {
   if (err) {
