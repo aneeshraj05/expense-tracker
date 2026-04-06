fcd4bc54b9b1789b2fbbd78f8ee97c9111c93c0c
aneeshraj05 <aneesh.rajx@gmail.com>
2026-04-07 01:11:21 +0530

    fixv bugs

--- backend/test-db.js
+++
@@ -1,7 +0,0 @@
-import db from './config/db.js';
-
-db.query('SELECT * FROM expenses LIMIT 1', (err, rows) => {
-  if (err) console.error(err);
-  console.log(JSON.stringify(rows, null, 2));
-  process.exit(0);
-});
